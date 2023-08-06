148c54cb4d645966ba0df824597161617ccc4a97
imjoseangel <josea.munoz@gmail.com>
2023-08-06 20:37:09 +0200

    add vpc

--- variables.tf
+++ variables.tf
@@ -41,3 +41,9 @@ variable "transit_gateway_attributes" {
     ])) == 0
   }
 }
+
+variable "cidr_block" {
+  description = "IPv4 CIDR range to assign to VPC if creating VPC or to associate as a secondary IPv6 CIDR. Overridden by var.vpc_id output from data.aws_vpc."
+  default     = null
+  type        = string
+}
